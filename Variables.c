2ee75fd1c7b185f669615ef8bca71e5b47365aec
mishrakomal9039-cell <mishrakomal9039@gmail.com>
2026-02-22 15:55:50 +0530

    Add Variables.c with basic variable operations
    Added a simple C program to demonstrate variable declaration and output.

---
+++ Variables.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Variables define karna
+    int a = 10;
+    int b = 20;
+    int sum = a + b;
+
+    // Output dikhana
+    printf("Pehla number: %d\n", a);
+    printf("Dusra number: %d\n", b);
+    printf("Dono ka total: %d", sum);
+
+    return 0;
+}
