3a5a201c18386d8fc5355c693a4ce3c18c10263a
mishrakomal9039-cell <mishrakomal9039@gmail.com>
2026-02-24 16:55:22 +0530

    Add age input and output functionality in input.c

--- input.c
+++ input.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    int age;
+    printf("Apni age enter karein: ");
+    
+    // User se input lene ke liye scanf ka use hota hai
+    scanf("%d", &age); 
+    
+    printf("Wow! Aap %d saal ke hain.", age);
+    return 0;
+}
